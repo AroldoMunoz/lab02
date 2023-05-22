357ec973f3c4bb92f3b0d4c662f78cc4067bf09d
Aroldo <83074122+AroldoMunoz@users.noreply.github.com>
2023-05-22 07:49:25 -0500

    Create ejer01.js
    creando ejercicio 01

---
+++ ejer01.js
@@ -0,0 +1,32 @@
+
+const d = new Date();
+let day = d.getDay()
+//day=5;
+//document.getElementById("demo").innerHTML = day;
+switch (day) {
+  case 1:
+    console.log("Lunes");
+    break;
+  case 2:
+    console.log("Martes");
+    break;
+  case 3:
+    console.log("Miercoles");
+    break;
+  case 4:
+    console.log("Jueves");
+    break;
+  case 5:
+    console.log("Viernes");
+    break; 
+  case 6:
+    console.log("Sabado");
+    break;  
+  case 7:
+    console.log("Domingo");
+    break;  
+  default:
+    break;
+}
+
+//console.log(day);
